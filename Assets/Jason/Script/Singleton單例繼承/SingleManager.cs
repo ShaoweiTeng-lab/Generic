388543d9916c4113123ae63d5747c73d8a49355e
鄧紹偉 <shaowei_teng@arplanet.com.tw>
2021-08-08 17:15:35 +0800

    add Singleton

---
+++ Assets/Jason/Script/Singleton單例繼承/SingleManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleManager : Singleton<SingleManager>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Test t1 = new Test();
+        t1.DoThing();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void SendMessage()
+    { 
+        Debug.Log("SingleManager : SendMessage");
+    }
+}
+public class Test
+{
+    public void DoThing()
+    {
+        SingleManager.instance.SendMessage();
+
+
+    }
+
+}
---
+++ Assets/Jason/Script/Singleton單例繼承/Singleton.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Component
+{
+	//繼承後即可將該腳本變為單例
+	#region Fields
+
+	/// <summary>
+	/// The instance.
+	/// </summary>
+	private static T _instance;
+
+	#endregion
+
+	#region Properties
+
+	/// <summary>
+	/// Gets the instance.
+	/// </summary>
+	/// <value>The instance.</value>
+	public static T instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = UnityEngine.Object.FindObjectOfType(typeof(T)) as T;
+				if (_instance == null)
+				{
+					GameObject obj = new GameObject(typeof(T).Name);
+					_instance = obj.AddComponent<T>();
+					DontDestroyOnLoad(obj);
+					Debug.Log(obj.name+"實例化");
+
+				}
+			}
+			return _instance;
+		}
+	}
+
+	#endregion
+
+	#region Methods
+
+	/// <summary>
+	///使用於初始化.
+	/// </summary>
+	/*protected virtual void Awake()
+	{
+		if (_instance == null)
+		{
+			_instance = this as T;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}*/
+
+	#endregion
+
+}
